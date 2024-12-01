bacc1ab845a94532905e26cfc2aa44a435a49206
NBDatsuya <2646809050@qq.com>
2024-12-02 04:49:06 +0800

    init commit

--- src/utils/system.ts
+++
@@ -1,8 +0,0 @@
-import {fileURLToPath, URL} from 'node:url'
-
-export const getStaticPath = () => {
-    if (process.env.NODE_ENV === 'production') {
-        return fileURLToPath(new URL('public/', import.meta.url))
-    }
-    return fileURLToPath(new URL('/', import.meta.url))
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
-import {getStaticPath} from "./src/utils/system";
 import {fileURLToPath, URL} from 'node:url'
 
 // https://vite.dev/config/
@@ -19,19 +18,19 @@ export default defineConfig({
     resolve: {
         alias: {
             '@':
-                fileURLToPath(new URL('./src', import.meta.url)),
+            new URL('./src', import.meta.url).path,
             '@assets':
-                fileURLToPath(new URL('./src/assets', import.meta.url)),
+            new URL('./src/assets', import.meta.url).path,
             '@config':
-                fileURLToPath(new URL('./src/config', import.meta.url)),
+            new URL('./src/config', import.meta.url).path,
             '@pages':
-                fileURLToPath(new URL('./src/pages', import.meta.url)),
-            '@portal':
-                fileURLToPath(new URL('./src/portal', import.meta.url)),
-            '@hooks':
-                fileURLToPath(new URL('./src/portal/hooks', import.meta.url)),
+            new URL('./src/pages', import.meta.url).path,
+            '@layout':
+            new URL('./src/portal', import.meta.url).path,
+            '@utils':
+            new URL('./src/portal/hooks', import.meta.url).path,
             '@static':
-                getStaticPath()
+            new URL('/', import.meta.url).path,
         }
     }
 })

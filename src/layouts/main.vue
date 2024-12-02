9b5be0c13ecae37b8ee484e03fb5405065fc10a9
NBDatsuya <2646809050@qq.com>
2024-12-02 08:29:49 +0800

    feat: main layout

---
+++ src/layouts/main.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import Sidebar from "@/components/layout/sidebar.vue";
+</script>
+
+<template>
+<div class="layout">
+	<sidebar />
+	<router-view/>
+</div>
+</template>
+
+<style scoped>
+@tailwind component;
+@layer component {
+	.layout {
+		@apply min-h-[100vh] flex flex-row
+	}
+}
+</style>

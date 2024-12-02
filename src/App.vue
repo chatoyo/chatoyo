2c000dec168748820d829f9c0a009886528bc216
NBDatsuya <2646809050@qq.com>
2024-12-02 08:28:25 +0800

    feat: ui-implementation begins

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,8 @@
 </script>
 
 <template>
-<div class="min-h-[100vh] flex flex-col justify-center p-4">
-	<h1 class="title-demo">如果你看到这一行字，并且这行字是粗体且宝石色，说明启动成功</h1>
-	<h1 class="title-demo">Welcome to ChatoYo</h1>
-</div>
+<router-view/>
 </template>
 
 <style scoped>
-@tailwind components;
-@layer components {
-	.title-demo {
-		@apply text-xl text-emerald-500 font-bold text-center
-	}
-}
 </style>

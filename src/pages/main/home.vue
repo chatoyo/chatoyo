5f2491caa91551e6cd2b1ea817f9398bac2e2d38
NBDatsuya <2646809050@qq.com>
2024-12-02 17:09:19 +0800

    feat: load card in home page

--- src/pages/main/home.vue
+++ src/pages/main/home.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
-
 import ChannelList from "@/components/channel/channel-list.vue";
 import ChannelDetail from "@/components/channel/channel-detail.vue";
+import HomeCard from "@/components/home/home-card.vue";
+
+const channelSelected = ref<{}>()
 </script>
 
 <template>
 <div class="home-page flex flex-row w-full">
-	<channel-list/>
-	<channel-detail class="flex-1"/>
+	<channel-list v-model:selected-channel="channelSelected"/>
+	<div class="main-container">
+		<channel-detail v-if="channelSelected" :channel="channelSelected"/>
+		<home-card v-else/>
+	</div>
 </div>
 </template>
 
 <style scoped>
-
+@tailwind components;
+@layer components {
+	.main-container {
+		@apply flex-1 bg-[#0c263a] h-screen w-full
+	}
+}
 </style>

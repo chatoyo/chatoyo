0066e61cdcfb440a79d661be3d92e26f358e3d4e
NBDatsuya <2646809050@qq.com>
2024-12-02 08:29:09 +0800

    feat: router-test, home page

---
+++ src/pages/main/home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import ChannelList from "@/components/channel/channel-list.vue";
+import ChannelDetail from "@/components/channel/channel-detail.vue";
+</script>
+
+<template>
+<div class="home-page flex flex-row w-full">
+	<channel-list/>
+	<channel-detail class="flex-1"/>
+</div>
+</template>
+
+<style scoped>
+
+</style>

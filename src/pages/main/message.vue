de80a5663ea899fbfda45ba49daf58ff59349837
NBDatsuya <2646809050@qq.com>
2024-12-03 18:24:27 +0800

    feat: message center page

--- src/pages/main/message.vue
+++ src/pages/main/message.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import List from "@components/list/list.vue";
 
+
+const message = ref([
+	{
+		id: 1,
+		title: '刘伟',
+		description: "你说得对，但是你说得对",
+		pic: 'https://webstatic.mihoyo.com/upload/op-public/2021/10/09/def1f2abcfc2af0bbe2e5900a60a5ee1_5699547505742166353.png'
+	}, {
+		id: 2,
+		title: '蔡浩宇',
+		description: "你说得对，但是你说得对",
+		pic: 'https://webstatic.mihoyo.com/upload/op-public/2021/10/09/870472d6104dbbe7ea18b27c13763ccb_5300916022261002498.png'
+	},
+])
 </script>
 
 <template>
-
+<div class="message-center">
+	<list :data="message" title="消息中心" subtitle="Message Center" class="w-[20rem]"/>
+</div>
 </template>
 
 <style scoped>

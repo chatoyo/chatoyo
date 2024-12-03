69c321ae4bba67c747030dd3840d5c21dc92f319
NBDatsuya <2646809050@qq.com>
2024-12-03 18:24:43 +0800

    config: component shortcut

--- vite.config.ts
+++ vite.config.ts
@@ -5,32 +5,34 @@ import {fileURLToPath, URL} from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            // imports: ['vue'],
-            eslintrc: {
-                enabled: true, // <-- this
-            },
-        }),
+	plugins: [
+		vue(),
+		AutoImport({
+			// imports: ['vue'],
+			eslintrc: {
+				enabled: true, // <-- this
+			},
+		}),
 
-    ],
-    resolve: {
-        alias: {
-            '@':
-                fileURLToPath(new URL('./src', import.meta.url)),
-            '@assets':
-                fileURLToPath(new URL('./src/assets', import.meta.url)),
-            '@config':
-                fileURLToPath(new URL('./src/config', import.meta.url)),
-            '@pages':
-                fileURLToPath(new URL('./src/pages', import.meta.url)),
-            '@layouts':
-                fileURLToPath(new URL('./src/layouts', import.meta.url)),
-            '@utils':
-                fileURLToPath(new URL('./src/utils', import.meta.url)),
-            '@static':
-                fileURLToPath(new URL('/', import.meta.url)),
-        }
-    }
+	],
+	resolve: {
+		alias: {
+			'@':
+				fileURLToPath(new URL('./src', import.meta.url)),
+			'@assets':
+				fileURLToPath(new URL('./src/assets', import.meta.url)),
+			'@config':
+				fileURLToPath(new URL('./src/config', import.meta.url)),
+			'@pages':
+				fileURLToPath(new URL('./src/pages', import.meta.url)),
+			'@layouts':
+				fileURLToPath(new URL('./src/layouts', import.meta.url)),
+			'@utils':
+				fileURLToPath(new URL('./src/utils', import.meta.url)),
+			'@static':
+				fileURLToPath(new URL('/', import.meta.url)),
+			'@components':
+				fileURLToPath(new URL('./src/components', import.meta.url))
+		}
+	}
 })

d1cbc231bc5fe2e7cd28cd2c52c51eac12c7207e
NBDatsuya <2646809050@qq.com>
2024-12-04 10:15:24 +0800

    project: enable prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,23 @@ module.exports = {
 	extends: [
 		'./.eslintrc-auto-import.json',
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended'
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended'
 	],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	root: true,
+	env: {
+		node: true,
+		browser: true,
+		es2021: true
+	},
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+			tsx: true
+		}
+	},
 }

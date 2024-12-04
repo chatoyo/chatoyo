1b3d0661e1ffa73fb033e163f3c8df096f5ded25
NBDatsuya <2646809050@qq.com>
2024-12-04 14:05:06 +0800

    reformatted: semicolon at the end of a expression

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
-import {createApp} from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 // Stylesheets
-import './assets/tailwind.css'
-import './style.css'
-import 'primeicons/primeicons.css'
+import './assets/tailwind.css';
+import './style.css';
+import 'primeicons/primeicons.css';
 
 // Dependencies
-import {createPinia} from "pinia";
-import router from "./router";
+import { createPinia } from 'pinia';
+import router from './router';
 
 // Dependency injection
-const pinia = createPinia()
+const pinia = createPinia();
 
 // Instance declarations
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-const instance = createApp(App)
-instance.use(pinia)
-instance.use(router)
-instance.mount('#app')
+const instance = createApp(App);
+instance.use(pinia);
+instance.use(router);
+instance.mount('#app');

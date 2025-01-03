821afe7e24ec07c0692ad26ee50c0af72f7ab3f5
黄旭达 <huangxuda@szkingdom.com>
2025-01-03 17:45:20 +0800

    config: enable auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,32 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import {fileURLToPath, URL} from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		AutoImport({
-			// imports: ['vue'],
-			eslintrc: {
-				enabled: true, // <-- this
-			},
-		}),
-
-	],
-	resolve: {
-		alias: {
-			'@':
-				fileURLToPath(new URL('./src', import.meta.url)),
-			'@assets':
-				fileURLToPath(new URL('./src/assets', import.meta.url)),
-			'@config':
-				fileURLToPath(new URL('./src/config', import.meta.url)),
-			'@pages':
-				fileURLToPath(new URL('./src/pages', import.meta.url)),
-			'@layouts':
-				fileURLToPath(new URL('./src/layouts', import.meta.url)),
-			'@utils':
-				fileURLToPath(new URL('./src/utils', import.meta.url)),
-			'@static':
-				fileURLToPath(new URL('/', import.meta.url)),
-			'@components':
-				fileURLToPath(new URL('./src/components', import.meta.url)),
-			'@models':
-				fileURLToPath(new URL('./src/models', import.meta.url)),
-			'@store':
-				fileURLToPath(new URL('./src/store', import.meta.url)),
-			'@services':
-				fileURLToPath(new URL('./src/services', import.meta.url)),
-		}
-	}
-})
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue'],
+      eslintrc: {
+        enabled: true // <-- this
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@config': fileURLToPath(new URL('./src/config', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@static': fileURLToPath(new URL('/', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@models': fileURLToPath(new URL('./src/models', import.meta.url)),
+      '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
+      '@services': fileURLToPath(new URL('./src/services', import.meta.url))
+    }
+  }
+});

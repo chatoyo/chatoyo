8f68fd348e2ee1625c3bad51c6f1595a84791cdd
黄旭达 <huangxuda@szkingdom.com>
2025-01-08 15:08:01 +0800

    feat: achievement.vue

---
+++ src/components/home/achievement.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="todo-list-component">
+    <ul class="list-core"></ul>
+    <div class="calendar"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@tailwind components;
+@layer components {
+  .todo-list-component {
+    @apply flex md:h-56 bg-red-300 w-6/12;
+  }
+}
+</style>

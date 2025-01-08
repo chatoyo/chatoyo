01f9d987c230135a9dbcd3732bd61b6f4116e308
黄旭达 <huangxuda@szkingdom.com>
2025-01-08 15:05:15 +0800

    dependency: prime ui and tailwindcss support

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,17 @@ import 'primeicons/primeicons.css';
 // Dependencies
 import pinia from './store';
 import router from './router';
+import PrimeVue from 'primevue/config';
 
 // Dependency injection
 
 // Instance declarations
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 const instance = createApp(App);
+
+instance.use(PrimeVue, {
+  theme: 'none'
+});
 instance.use(pinia);
 instance.use(router);
 instance.mount('#app');

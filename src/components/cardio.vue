63c32f1adf5e9234808ebb61389772d32d74be0e
黄旭达 <huangxuda@szkingdom.com>
2025-01-13 07:47:03 +0800

    feat: add mode-related name display support in cardio(status bar)

--- src/components/cardio.vue
+++ src/components/cardio.vue
@@ -2,20 +2,20 @@
 <!-- Named by Archie Xing -->
 <script setup lang="ts">
 import { useModeStore } from '@store/mode-store.ts';
-import { computed } from 'vue';
-import Toggle from './minor-custom/labeled-toggle.vue';
-import { Slider } from 'primevue';
+import Toggle from '@components/minor-custom/labeled-toggle.vue';
 
 type CardioProps = {
   avatar?: string;
   name?: string;
+  nickname?: string;
   sex?: number;
   slogan?: string;
 };
 
 const props = withDefaults(defineProps<CardioProps>(), {
   avatar: '/images/avatar-test.jpg',
-  name: '用牌玩命',
+  nickname: '用牌玩命',
+  name: '青雀',
   sex: 1,
   slogan: '哎呀，这帝元琼玉瘾又犯了......'
 });
@@ -26,13 +26,14 @@ const sexSymbol = {
 };
 const modeStore = useModeStore();
 const isWorkMode = computed(() => modeStore.workMode === 'WORK');
+const username = computed(() => (isWorkMode ? props.name : props.nickname));
 </script>
 
 <template>
   <div class="cardio">
     <div class="avatar"><img class="avatar-img" :src="props.avatar" /></div>
     <div class="title">
-      <span>{{ props.name }}</span>
+      <span>{{ username }}</span>
       <span class="sex" :class="`bg-${sexSymbol[props.sex as 1 | 0].color}-500`">{{
         sexSymbol[props.sex as 1 | 0].text
       }}</span>
@@ -52,9 +53,7 @@ const isWorkMode = computed(() => modeStore.workMode === 'WORK');
     </div>
     <div class="mode-toggle">
       <div class="relative flex flex-col flex-wrap items-center gap-2">
-        <Toggle id="id-c04" :model-value="isWorkMode" :disabled="false">
-          工作模式
-        </Toggle>
+        <Toggle id="id-c04" :model-value="isWorkMode" :disabled="false"> 工作模式</Toggle>
       </div>
     </div>
   </div>
@@ -63,7 +62,6 @@ const isWorkMode = computed(() => modeStore.workMode === 'WORK');
 <style scoped lang="scss">
 @tailwind components;
 @layer components {
-
   .cardio {
     @apply bg-amber-50 dark:bg-sky-900
     flex gap-4 items-center pe-6
@@ -109,7 +107,6 @@ const isWorkMode = computed(() => modeStore.workMode === 'WORK');
 
   .stat-item {
     @apply flex flex-col items-end;
-    }
-
+  }
 }
 </style>
